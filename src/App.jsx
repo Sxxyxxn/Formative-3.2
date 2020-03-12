519df6e6f630395f1500014116e8c0e804a8dfb2
Ella <sycoalla21@gmail.com>
2020-03-13 11:41:47 +1300

    server update

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "./css/home.css";
 import { Link } from "@reach/router";
 import Nav from "./Components/nav";
 // import chef from "../images/chef.png";
 import chef from "./images/chef.png";
+import { render } from "@testing-library/react";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="home_screen">
-        <Nav />
+export default class App extends Component {
+  componentDidMount() {
+    Axios.post("http://localhost:4002/api/users").then(res => {
+      console.log(res.data);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="home_screen">
+          <Nav />
 
-        <div className="categories-main">
-          <Link className="my-links" to="/gardner">
-            <div className="section-1"> </div>
-          </Link>
+          <div className="categories-main">
+            <Link className="my-links" to="/gardner">
+              <div className="section-1"> </div>
+            </Link>
 
-          <Link className="my-links" to="/cleaner">
-            <div className="section-2"></div>
-          </Link>
+            <Link className="my-links" to="/cleaner">
+              <div className="section-2"></div>
+            </Link>
 
-          <Link className="my-links" to="/chef">
-            <div className="section-3"></div>
-          </Link>
+            <Link className="my-links" to="/chef">
+              <div className="section-3"></div>
+            </Link>
 
-          <Link className="my-links" to="/watchman">
-            <div className="section-4"></div>
-          </Link>
+            <Link className="my-links" to="/watchman">
+              <div className="section-4"></div>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
